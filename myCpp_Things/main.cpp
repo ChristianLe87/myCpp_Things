1895ee7d44d1f2185dbffa5c3ff849062ef18058
Christian <christianle87@icloud.com>
2020-05-28 19:49:33 +0200

    fix things

--- myCpp_Things/main.cpp
+++ myCpp_Things/main.cpp
@@ -1,7 +1,4 @@
-
-
 #include <iostream>
-
 
 struct Node{
     public: int number;
@@ -24,16 +21,23 @@ class List{
         }
         else
         {
-            // create new node
-            Node *newNode = new Node();
-            newNode->number = number;
-            newNode->nextNode = NULL;
-            
             Node* node = firstNode;
-            node->nextNode = newNode;
+            
+            while (true) {
+                if(node->nextNode == NULL){
+                    // create new node
+                    Node *newNode = new Node();
+                    newNode->number = number;
+                    newNode->nextNode = NULL;
+                    
+                    node->nextNode = newNode;
+                    break;
+                }else{
+                    node = node->nextNode;
+                }
+            }
         }
     }
-    
 };
 
 
@@ -44,16 +48,26 @@ int main(int argc, const char * argv[]) {
     List list;
     list.Add(11);
     list.Add(22);
-    //list.Add(33);
-    //list.Add(44);
+    list.Add(33);
+    list.Add(44);
     
     
     Node n1 = *list.firstNode;
     int firstElement = n1.number;
-    
+    std::cout<< firstElement << "\n";
+
     
     Node n2 = *n1.nextNode;
     int secondElement = n2.number;
+    std::cout<< secondElement << "\n";
+    
+    Node n3 = *n2.nextNode;
+    int thirdElement = n3.number;
+    std::cout<< thirdElement << "\n";
+    
+    Node n4 = *n3.nextNode;
+    int fourthElement = n4.number;
+    std::cout<< fourthElement << "\n";
     
     printf("\n-- end --\n");
     return 0;
